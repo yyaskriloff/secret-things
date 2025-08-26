6286ca0bc5ad6364b0536c92a309f83af766f143
Aaron Skriloff <69407772+yyaskriloff@users.noreply.github.com>
2025-08-26 15:30:56 +0300

    add sudo code structure

--- main.go
+++ main.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
+
+	// parse arguments: profile, congig, env (set, load, sync flags env, application)
+	// switch per command
+	// profile - name
+	// config app - name flags dir?
+	// env create - name
+	// env delete - name
+	//  set - name, value
+	// load path env?
+	// sync dir? env? file?
+
 }
